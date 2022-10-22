50663954f60f47aff8c5999eda675c33df64f29a
Adrion10 <lacehilaQgmail.com>
2022-10-22 16:11:37 +0200

    work component

--- frontend_react/src/constants/images.js
+++ frontend_react/src/constants/images.js
@@ -35,6 +35,7 @@ import nb from "../assets/nb.png";
 import skype from "../assets/skype.png";
 import spotify from "../assets/spotify.png";
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default {
   email,
   mobile,
