beea91c560f353fcfc7153797a7d018c0f0535c0
Adrion10 <lacehilaQgmail.com>
2022-10-23 23:18:59 +0200

    fixed

--- frontend_react/src/index.js
+++ frontend_react/src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+const root = createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
