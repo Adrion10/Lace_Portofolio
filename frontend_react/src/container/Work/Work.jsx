849fec3a2dcaf2e03ae1dbdb68d68163ee4b4317
Adrion10 <lacehilaQgmail.com>
2022-10-23 15:14:27 +0200

    work map

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -1,13 +1,35 @@
 
 import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
-import {motion} from 'framer-motion'
+import { motion } from 'framer-motion'
+
+import {AppWrap} from '../../client'
 
 import './Work.scss'
 
 const Work = () => {
+
+  const [activeFilter, setActiveFilter] = useState('All')
+  const handleWorkFilter = (item) => {
+    
+  }
   return (
-    <div>Work</div>
+    <>
+     <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
+
+<div className="app__work-filter">
+  {['UI/UX', 'Web App', 'React JS', 'MERN', 'All'].map((item, index) => (
+    <div
+      key={index}
+      onClick={() => handleWorkFilter(item)}
+      className={`app__work-filter-item app__flex p-text ${activeFilter === item ? 'item-active' : ''}`}
+    >
+      {item}
+    </div>
+  ))}
+</div>
+      
+    </>
   )
 }
 
