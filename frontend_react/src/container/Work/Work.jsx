42497e1ffdea2122b96aba9a78a075bbcef8b48c
Adrion10 <lacehilaQgmail.com>
2022-10-23 19:52:54 +0200

    work

--- frontend_react/src/container/Work/Work.jsx
+++ frontend_react/src/container/Work/Work.jsx
@@ -3,16 +3,28 @@ import React, { useState, useEffect } from 'react'
 import { AiFillEye, AiFillGithub } from 'react-icons/ai'
 import { motion } from 'framer-motion'
 
-import {AppWrap} from '../../client'
+import { AppWrap } from '../../Wrapper';
+import { urlFor, client } from '../../client';
 
 import './Work.scss'
 
 const Work = () => {
 
   const [activeFilter, setActiveFilter] = useState('All')
-  const handleWorkFilter = (item) => {
-    
-  }
+  const [animateCard, setanimateCard] = useState({ y: 0, opacity: 1 })
+  const [works, setWorks] = useState([])
+  const [filterWork, setFilterWork] = useState([])
+  
+useEffect(() => {
+  const query = '*[_type == "works]';
+  client.fetch(query).then((data) => {
+    setWorks(data)
+    setFilterWork(data)
+  })
+}, [])
+
+
+  
   return (
     <>
      <h2 className="head-text">My Creative <span>Portfolio</span> Section</h2>
@@ -28,6 +40,21 @@ const Work = () => {
     </div>
   ))}
 </div>
+      <motion.div
+        animate={animateCard}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className="app__work-protfolio"
+      >
+        {filterWork.map((work, index) => (
+          <div className='app__work-item app__flex'
+            key={index}>
+            <div className='app__work-img app_flex'>
+              <img src={urlFor(work.imgUrl)} alt={work.name} />
+
+            </div>
+       </div>
+        ))}
+      </motion.div>
       
     </>
   )
