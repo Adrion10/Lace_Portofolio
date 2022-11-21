7b72f9b1150b4bd7c09c27611d731f806f1bc309
Adrion10 <lacehila@gmail.com>
2022-11-21 21:39:57 +0100

    className="app__footer-card">

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -4,7 +4,16 @@ import { images } from "../../constants";
 import { AppWrap, MotionWrap } from "../../Wrapper";
 import { client } from "../../client";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <>
+      <h2 className="head-text">Take a coffe & chat with me</h2>
+      <div className="app__footer-cards">
+        <div className="app__footer-card">
+          <img src={images.email} alt="email" />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AppWrap(
