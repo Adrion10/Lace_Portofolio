99ae117e24cc0fbe5a03c4759d243d93e2910eb6
Adrion10 <lacehila@gmail.com>
2022-11-21 21:32:10 +0100

    export default AppWrap(   MotionWrap(Footer, "app__footer"),   "contact",   "app_whitebg" );

--- frontend_react/src/container/Footer/Footer.jsx
+++ frontend_react/src/container/Footer/Footer.jsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import "./Footer.scss";
 import { images } from "../../constants";
-
+import { AppWrap, MotionWrap } from "../../Wrapper";
+import { client } from "../../client";
 const Footer = () => {
   return <div>Footer</div>;
 };
 
-export default Footer;
+export default AppWrap(
+  MotionWrap(Footer, "app__footer"),
+  "contact",
+  "app_whitebg"
+);
